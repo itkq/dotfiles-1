e5e0c6b65f636f127172d3f7c12bfc60e6b25bdc
Takuya Kosugiyama <kuogsi@gmail.com>
2017-07-14 23:25:56 +0900

    zsh: Update theme

--- config/.zsh/lib/theme.zsh
+++ config/.zsh/lib/theme.zsh
@@ -1,24 +1,34 @@
 autoload -Uz vcs_info
 
+function __current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
+
 precmd() {
   local last_status="$?"
   LANG=en_US.UTF-8 vcs_info
 
-  local left1="$(pwd) "
+  local left1="$(pwd | sed -e "s#$HOME#~#") "
   local left2="[$last_status]"
-  local right1="$vcs_info_msg_0_"
+  local left3=" $(__current_branch) "
+  local right1="[$(date +"%H:%M:%S")]"
+  local right2="$vcs_info_msg_0_"
 
   psvar=()
   psvar[1]="${left1}"
   psvar[2]="${left2}"
-  psvar[3]="${(r:($COLUMNS-${#left1}-${#left2}-${#right1}):: :)}"
-  psvar[4]="${right1}"
+  psvar[3]="${left3}"
+  psvar[4]="${(r:($COLUMNS-${#left1}-${#left2}-${#left3}-${#right1}):: :)}"
+  psvar[5]="${right1}"
+  psvar[6]="${right2}"
 
   if [[ "x$last_status" == "x0" ]]; then
-    PROMPT="%F{cyan}%1v%f%2v%3v%F{green}%4v%f
+    PROMPT="%F{cyan}%1v%f%2v%3v%4v%F{green}%5v%f
 $ "
   else
-    PROMPT="%F{cyan}%1v%f%F{red}%2v%f%3v%F{green}%4v%f
+    PROMPT="%F{cyan}%1v%f%F{red}%2v%f%3v%4v%F{green}%5v%f
 $ "
   fi
 }
