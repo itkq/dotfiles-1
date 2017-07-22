bc0b0ddbba736d234bd13bfaa519a972acb76c22
Takuya Kosugiyama <kuogsi@gmail.com>
2017-07-22 16:35:52 +0900

    zsh: Add google-cloud-sdk path

--- config/.zsh/lib/go.zsh
+++ config/.zsh/lib/go.zsh
@@ -2,3 +2,9 @@
 export GOPATH=$HOME
 export PATH="${GOPATH}/bin:$PATH"
 export GOMAXPROCS=4
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/itkq/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/itkq/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/itkq/google-cloud-sdk/completion.zsh.inc' ]; then source '/Users/itkq/google-cloud-sdk/completion.zsh.inc'; fi
