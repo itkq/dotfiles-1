e112144d41c469651f79a7558f7cac30734eea56
itkq <kuogsi@gmail.com>
2018-01-25 13:48:42 +0900

    Karabiner-Elements default

--- cookbooks/karabiner/default.rb
+++ cookbooks/karabiner/default.rb
@@ -3,9 +3,5 @@ node.reverse_merge!(
 )
 
 if node[:macos_major_version] >= 12 # sierra
-  install_dmg 'https://pqrs.org/latest/karabiner-elements-latest.dmg' do
-    not_cond "[ -e /Applications/Karabiner-Elements.app ]"
-  end
-
   dotfile ".config/karabiner"
 end
