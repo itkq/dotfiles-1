eadac96b3a7bb95320e0a01aaaa0e914df869fcd
itkq <kuogsi@gmail.com>
2018-03-10 01:56:58 +0900

    zsh: autojump

--- config/.zsh/lib/bundle.zsh
+++ config/.zsh/lib/bundle.zsh
@@ -1,3 +1,4 @@
 [[ -e ~/.zsh/bundle/enhancd/init.sh ]] && source ~/.zsh/bundle/enhancd/init.sh
 [[ -e ~/.zsh/bundle/zsh-syntax-highlightning/zsh-syntax-highlighting.zsh ]] && source ~/.zsh/bundle/zsh-syntax-highlightning/zsh-syntax-highlighting.zsh
 [[ -e ~/.zsh/bundle/zsh-completions/zsh-completions.plugin.zsh ]] && source ~/.zsh/bundle/zsh-completions/zsh-completions.plugin.zsh
+[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh # homebrew
--- cookbooks/homebrew/default.rb
+++ cookbooks/homebrew/default.rb
@@ -3,6 +3,7 @@ package 'openssl'
 package 'jid'
 package 'gist'
 package 'direnv'
+package 'autojump'
 
 package 'global' do
   options '--with-exuberant-ctags --with-pygments'
