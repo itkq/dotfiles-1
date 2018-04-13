6e424b5c336e3c4dadcb625ec5680b1403f4cf0a
itkq <kuogsi@gmail.com>
2018-04-13 10:04:13 +0900

    Remove root

--- cookbooks/zsh/default.rb
+++ cookbooks/zsh/default.rb
@@ -21,7 +21,7 @@ dotfile '.zshrc.titech'
 
 execute "chsh -s /bin/zsh #{node[:user]}" do
   only_if "getent passwd #{node[:user]} | cut -d: -f7 | grep -q '^/bin/bash$'"
-  user 'root'
+  # user 'root'
 end
 
 execute 'Install zplug' do
