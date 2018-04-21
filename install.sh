2d831cafee5afa524a0ad4be0afdea22011980cf
itkq <kuogsi@gmail.com>
2018-04-21 16:36:51 +0900

    xcode initial

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ bin/setup
 case "$(uname)" in
   "Darwin")
     ! which brew >/dev/null && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    ! [ -d /Applications/Xcode.app ] && sudo xcode-select --install
     bin/mitamae local $@ lib/recipe.rb ;;
   *) sudo -E bin/mitamae local $@ lib/recipe.rb ;;
 esac
