e4d0fcefa72466e5c0becbc194f9f982ad9b7043
itkq <kuogsi@gmail.com>
2018-05-01 15:18:05 +0900

    gpg-agent

--- cookbooks/gnupg/default.rb
+++ cookbooks/gnupg/default.rb
@@ -1,16 +1,9 @@
 package 'gpg2' do
   not_if 'which gpg > /dev/null'
 end
-package 'gpg-agent' do
-  notifies :create, 'file[~/.gnupg/gpg-agent.conf]'
-end
-package 'pinentry-mac'
 
-
-file "#{ENV['HOME']}/.gnupg/gpg-agent.conf" do
-  action :nothing
-  content <<EOS
-use-standard-socket
-pinentry-program /usr/local/bin/pinentry-mac
-EOS
+%w(pinentry-mac gpg-agent).each do |pkg|
+  package pkg
 end
+
+dotfile '.gnupg/gpg-agent.conf'
