6debcbda11fcd550898aa62a054d463d246101ea
Takuya Kosugiyama <re@itkq.jp>
2021-01-02 19:49:31 +0900

    Update gcloud setup

--- config/.zsh/lib/gcloud.zsh
+++ config/.zsh/lib/gcloud.zsh
@@ -1,7 +1,7 @@
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/takuya-kosugiyama/Downloads/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/takuya-kosugiyama/Downloads/google-cloud-sdk/path.zsh.inc'; fi
+if [ -f "$HOME/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/google-cloud-sdk/path.zsh.inc"; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f '/Users/takuya-kosugiyama/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/takuya-kosugiyama/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
+if [ -f "$HOME/google-cloud-sdk/completion.zsh.inc" ]; then . "$HOME/google-cloud-sdk/completion.zsh.inc"; fi
 
 alias g=gcloud
