deea6385ee8488bbc7d564dd74fdac36eb8373ee
Takuya Kosugiyama <re@itkq.jp>
2021-01-06 19:58:51 +0900

    Tidy up

--- config/.zsh/lib/aliases.zsh
+++ config/.zsh/lib/aliases.zsh
@@ -1,6 +1,3 @@
-# override system ctags
-# alias ctags="/usr/local/bin/ctags"
-
 # .zshrc reload
 alias re="source ~/.zshrc"
 
@@ -13,25 +10,16 @@ function vi() {
 export PATH="/opt/brew/bin:/usr/local/bin:${PATH}"
 export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 
-# cd by dir name only for ..
-alias ..="cd .."
-
-alias l=ll
-alias lll=ll
-
-# which nvim > /dev/null && alias vim=nvim && export EDITOR=nvim
 
 function mkcd() {
   mkdir $1
   cd $1
 }
 
-which pget > /dev/null && alias wget=pget
 alias d="direnv allow"
 alias de="direnv edit"
 
 alias k="kubectl"
 
-alias l="ls -l"
-alias ll="ls -l"
-alias lll="ls -l"
+alias l=ll
+alias lll=ll
--- config/.zsh/lib/ruby.zsh
+++ config/.zsh/lib/ruby.zsh
@@ -8,14 +8,4 @@ alias be="bundle exec"
 # bundle open
 export BUNDLER_EDITOR="vim"
 
-# rails
-alias -g RET="RAILS_ENV=test"
-alias -g RED="RAILS_ENV=development"
-alias -g REP="RAILS_ENV=production"
-
-# hash rocket
-function hr() {
-  sed -i '' -e 's/:\([a-zA-Z_]*\) =>/\1:/g' $1
-}
-
 export DISABLE_SPRING=1
