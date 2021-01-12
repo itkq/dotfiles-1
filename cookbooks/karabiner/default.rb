37c5fe175bdc20971d017cc6a035f23b1b2782d8
Takuya Kosugiyama <re@itkq.jp>
2021-01-12 12:59:00 +0900

    Enable karabiner

--- cookbooks/karabiner/default.rb
+++ cookbooks/karabiner/default.rb
@@ -2,6 +2,4 @@ node.reverse_merge!(
   macos_major_version: `sw_vers | grep 'ProductVersion' | awk '{print $2}'`.split(".")[1].to_i
 )
 
-if node[:macos_major_version] >= 12 # sierra
-  dotfile ".config/karabiner"
-end
+dotfile ".config/karabiner/karabiner.json"
